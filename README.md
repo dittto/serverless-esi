a99ce4d29e2fa17c9d5519763e267e5ddcb426f4
Nick Stacey <nick@ditt.to>
2016-10-03 09:06:32 +0100

    Testing better code

---
+++ README.md
@@ -0,0 +1,7 @@
+- work out number of pages to read and send to sqs with time delay
+
+- use sns to trigger from sqs after delay
+
+- read each page, translate, sns (do we need this sns level?)
+
+- read each piece of data and send to db?
--- handler.js
+++ handler.js
@@ -1,5 +1,58 @@
 'use strict';
 
+class ESIRequest {
+    constructor(s3, defaultParams) {
+        this.s3 = s3;
+        this.defaultParams = defaultParams;
+    }
+
+    sendRequest(paths) {
+        let promises;
+        for (let path of paths) {
+            promises[promises.length] = new Promise((resolve, reject) => {
+                this.promiseDeclaration(path, resolve, reject);
+            });
+        }
+
+        return new Promise((resolve, reject) => {
+            Promise.all(promises).then(values => {
+                console.log("params");
+                console.log(values);
+
+                // read body and retrieve esi urls
+
+                // next two steps in a sub function
+
+                    // send request on all new esi urls
+
+                    // on a valid response, replace urls with body code
+
+                // how to tag esi code correctly?
+
+                // resolve(other_vals);
+            }, reason => {
+                console.log("reason");
+                console.log(reason);
+                // reject(reasons);
+            });
+        });
+    }
+
+    promiseDeclaration(path, resolve, reject) {
+        let params = Object.assign(this.defaultParams, {Key: path});
+
+        // an s3 getObject request for each path
+        this.s3.getObject(
+            params, (err, data) => {
+                console.log("params");
+                // on error, reject
+
+                // otherwise, resolve with path and body
+            }
+        );
+    }
+}
+
 module.exports.esi = (event, context, cb) => {
 
     // get the path
@@ -19,8 +72,26 @@ module.exports.esi = (event, context, cb) => {
         Key: path
     };
 
+    // send esi request
+    let esiRequest = new ESIRequest(s3, params);
+    let promise = esiRequest.sendRequest(['test.html']);
+    promise.then(values => {
+        console.log("values");
+    }, reason => {
+        console.log("reason");
+    });
+
+    /*
     s3.getObject(
         params, (err, data) => {
+            // parse received code for esi urls
+
+            // send esi requests in batch
+
+            // return html
+
+
+
             if (err) {
                 console.log("GET ERROR");
                 cb(new Error('[404] Missing file at ' + path + ' ' + err));
@@ -29,8 +100,12 @@ module.exports.esi = (event, context, cb) => {
                 let body = data.Body.toString('utf-8');
                 cb(null, body);
             }
+
+
+
         }
-    );
+    );*/
+    cb(null, "done");
 
     // register of current getObjects when all success, return data
 
